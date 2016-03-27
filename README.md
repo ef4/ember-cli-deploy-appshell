f351ed028ddc87d63f4b641ed2a1d89e2551d2e5
Edward Faulkner <ef@alum.mit.edu>
2016-03-27 06:08:30 -0400

    initial working release

--- README.md
+++ README.md
@@ -1,26 +1,29 @@
-# Ember-app-shell
+# ember-app-shell
 
-This README outlines the details of collaborating on this Ember addon.
+This is an ember-cli-deploy plugin that loads your Ember application
+via an `applicationCache`-based bootloader. It
 
-## Installation
+ - reliably caches all your assets for speed and offline use
+ - always boots your freshest version when the network is sufficiently fast
+ - never blocks application boot to preemptively cache assets
 
-* `git clone` this repository
-* `npm install`
-* `bower install`
+## Working example
 
-## Running
 
-* `ember server`
-* Visit your app at http://localhost:4200.
 
-## Running Tests
+## Key requirements:
 
-* `npm test` (Runs `ember try:testall` to test your addon against multiple Ember versions)
-* `ember test`
-* `ember test --server`
+ 1. Set `autoRun: false` in the arguments to `new EmberApp` in your `ember-cli-build.js` file. Our bootloader code takes over deciding when to boot your app.
 
-## Building
+ 2. Remove `loader.js` from your package.json, and set `_ignoreMissingLoader: true` in the arguments to `new EmberApp` in your `ember-cli-build.js` file. This addon inlines its own copy of `loader.js` in order to manage ambiguity between multiple versions of your app that may be attempting to load in parallel.
 
-* `ember build`
+ 3. Wherever you are deploying `index.html`, you must also deploy `appshell.html` and `appcache.manifest`.
 
-For more information on using ember-cli, visit [http://www.ember-cli.com/](http://www.ember-cli.com/).
+ 4. Make sure you're using fingerprinted assets. This is critical to busting through the application cache to get instant updates when online.
+
+## What is appshell.html?
+
+It's a nearly-identical copy of your index.html, with one key difference: it lacks a `manifest` attribute, so it doesn't get stored in `applicationCache`. We use it to bust through the cache when we want to check for the latest index. If we find a new index, we shove it into DOM and let it take over immediately, while the normal `applicationCache` update is happening in the background.
+
+
+
--- index.js
+++ index.js
@@ -1,6 +1,66 @@
 /* jshint node: true */
-'use strict';
+"use strict";
+let path = require('path');
+let uglify = require('uglify-js');
+let DeployPluginBase = require('ember-cli-deploy-plugin');
+let fs = require('fs');
+let minimatch = require('minimatch');
 
 module.exports = {
-  name: 'ember-app-shell'
+  name: 'ember-cli-deploy-appshell',
+  isDevelopingAddon() {
+    return true;
+  },
+
+  included(app) {
+    this.app = app;
+  },
+
+  contentFor(type, config) {
+    if (type === 'head') {
+      let loader = fs.readFileSync(require.resolve('loader.js'), 'utf8');
+      let src = fs.readFileSync(path.join(__dirname, 'lib', 'bootloader.js'), 'utf8').replace(/MODULE_PREFIX/g, config.modulePrefix);
+      var bootloader = uglify.minify(loader + src, { fromString: true, mangle:true, compress: true}).code;
+      return `<script type="text/javascript">${ bootloader }</script>`;
+    }
+  },
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = DeployPluginBase.extend({
+      name: options.name,
+      didBuild: function(context) {
+        let distDir = context.distDir;
+        let files = context.distFiles;
+        fs.writeFileSync(path.join(distDir, 'manifest.appcache'), this.renderManifest(files));
+        let indexHTML = fs.readFileSync(path.join(distDir, 'index.html'), 'utf8');
+        fs.writeFileSync(path.join(distDir, 'appshell.html'), indexHTML);
+        context.distFiles.push('appshell.html');
+        fs.writeFileSync(path.join(distDir, 'index.html'), indexHTML.replace(/<html>/i, '<html manifest="/manifest.appcache">'));
+      },
+      renderManifest: function(paths) {
+        let excludePattern = this.readConfig('excludePattern');
+        let prefixDomains = this.readConfig('prefixDomains');
+        let outputPaths = paths.filter(function(p){ return !minimatch(p, excludePattern); })
+            .map(function(p) {
+              let domain = Object.keys(prefixDomains).find(function(domain) {
+                return minimatch(p, prefixDomains[domain]);
+              });
+              if (domain) {
+                return domain + p;
+              } else {
+                return p;
+              }
+            });
+        outputPaths = outputPaths.concat(this.readConfig('externalDependencies'));
+
+        return `CACHE MANIFEST
+# ${new Date()}
+${ outputPaths.join("\n") }
+NETWORK:
+*`;
+      }
+    });
+    return new DeployPlugin();
+  }
+
 };
